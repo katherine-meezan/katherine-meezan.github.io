81f1d9ab05af40337587dc7f5d481e955ac1f10e
zboy02 <zacheryboyer02@gmail.com>
2025-11-04 22:37:00 -0800

    Update sensor_array.py

--- Lab 0x04/on_board/sensor_array.py
+++ Lab 0x04/on_board/sensor_array.py
@@ -15,7 +15,7 @@ class sensor_array:
         for i in range(100):
             for j in range(len(self.sensors)):
             reads_calib += 1
-            sums += self.sensors[j].read()
+                sums += self.sensors[j].read()
         for i in range(len(self.sensors)):
             self.sensors[i].set_black(sums[i]/reads_calib)
     def calibrate_white(self):
@@ -24,9 +24,19 @@ class sensor_array:
         for i in range(100):
             for j in range(len(self.sensors)):
             reads_calib += 1
-            sum += self.sensors[j].read()
+                sum += self.sensors[j].read()
         for i in range(len(self.sensors)):
             self.sensors[i].set_white(sums[i] / reads_calib)
+
+    def find_centroid(self):
+        sum_vals = 0
+        weighted_sum = 0
+        for i in range(len(self.sensors)):
+            raw_val = self.sensors[i].read()
+            self.norm_val = (self.sensors[i].get_white()-white)/(self.sensors[i].get_white-self.sensors[i].get_black())
+            sum_vals += self.norm_val
+            weighted_sum += self.norm_val*i
+        self.centroid = weighted_sum/sum_vals
     
     def sensor(self):
         

ce77ea5fa3cb8e86c0ed1eaa5b71218d608c3713
katherine-meezan <katherinemeezan@gmail.com>
2025-12-10 20:21:04 -0800

    Modify config

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,10 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
+
+html_theme = "sphinx_rtd_theme"
+
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:

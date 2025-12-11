084dc35ad84be62699835d5302fb5b47631d7bb9
katherine-meezan <katherinemeezan@gmail.com>
2025-12-10 20:15:14 -0800

    Docs files for RTD

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ME405_FinalProject'
+copyright = '2025, Katherine Meezan, Michael Habib, Zachery Boyer'
+author = 'Katherine Meezan, Michael Habib, Zachery Boyer'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. ME405_FinalProject documentation master file, created by
+   sphinx-quickstart on Wed Dec 10 20:14:16 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ME405_FinalProject documentation
+================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
